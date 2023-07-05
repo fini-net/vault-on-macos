c00c6a2a24433e6ef87e8a0ff9d144ea7e6ebf8a
Christopher Hicks <chicks.net@gmail.com>
2023-07-04 23:14:07 -0700

    script for installing vault

---
+++ install-vault.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+brew install vault
+
+echo "Don't use the vault service installed by brew unless you want dev mode."
+# TODO: remove brew's vault service with dev mode, or rename it
